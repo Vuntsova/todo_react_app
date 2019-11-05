3b228394bbf2373da9d5670034243a11f186a12c
Emiliya <emiliya@Emiliyas-MacBook-Pro.local>
2019-11-05 09:52:02 -0500

    gitignore added

---
+++ build/precache-manifest.bc1c981ccf85bd3e48857f9b85a0041f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b6b27adf9a83aed5b38b8d6bb1afb6d",
+    "url": "/todo_react_app/index.html"
+  },
+  {
+    "revision": "2b00c5dd914b00f731b7",
+    "url": "/todo_react_app/static/css/main.34de6062.chunk.css"
+  },
+  {
+    "revision": "d6a86ecafc5f9a061acb",
+    "url": "/todo_react_app/static/js/2.0bf678a4.chunk.js"
+  },
+  {
+    "revision": "2b00c5dd914b00f731b7",
+    "url": "/todo_react_app/static/js/main.225cee4e.chunk.js"
+  },
+  {
+    "revision": "0bd8f6d694f51e784dff",
+    "url": "/todo_react_app/static/js/runtime-main.6abe4aa6.js"
+  }
+]);
